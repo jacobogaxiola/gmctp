1dece2934c4773f7a0bb7e10e5b26e0ebc9df4a6
jacobogaxiola <jacobogaxiola@gmail.com>
2024-03-08 15:07:04 -0700

    Manejo de estado

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Grid, Paper, TextField, FormControlLabel, Checkbox, Button, Typography, Link} from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import LockPersonOutlinedIcon from '@mui/icons-material/LockPersonOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
@@ -9,6 +9,23 @@ const Login=()=>{
     const textFieldStyle={margin: "15px 0 0 0 "}
     const buttonStyle={backgroundColor:"orange"}
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onEmailChange = (e)=>{
+        let email=e.target.value
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(email.match(validRegex)) {
+            console.log("Correo ok");
+            setEmail(email);
+        }
+        else
+            console.log("Correo inválido");
+    }
+    const onLogin = ()=>{
+        
+    }
+
     return(
         <Grid>
             <Paper elevation={10} style={paperStyle}>
@@ -17,10 +34,29 @@ const Login=()=>{
                     <h2>GMT C-TPAT</h2>
                 </Grid>
   
-                <TextField label="Correo Electrónico" placeholder="Teclea tu correo..." variant="outlined" fullWidth required />
-                <TextField label="Contraseña" style={textFieldStyle}  placeholder="Teclea tu contraseña..." type="password" variant="outlined" fullWidth required />
+                <TextField 
+                    label="Correo Electrónico" 
+                    placeholder="Teclea tu correo..." 
+                    variant="outlined" fullWidth required 
+                    onChange={onEmailChange}
+                />
+                <TextField 
+                    label="Contraseña" 
+                    style={textFieldStyle}  
+                    placeholder="Teclea tu contraseña..." 
+                    type="password" 
+                    variant="outlined" fullWidth required
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <FormControlLabel  control={<Checkbox name="checkedB" color="primary"/>} label="Recordarme"/>
-                <Button type='submit' color='primary' variant="contained" style={buttonStyle} fullWidth>Acceder</Button>
+                <Button 
+                    type='submit' 
+                    color='primary' 
+                    variant="contained" 
+                    style={buttonStyle} 
+                    fullWidth
+                >Acceder
+                </Button>
                 <Typography>
                     <Link href="#">
                         ¿Olvidaste tu contraseña?
