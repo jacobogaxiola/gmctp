49ffc513b97bc278b0b2b989690e0c34d4b4a21e
Diegolpz18 <diego.jared.leyva@cecytesinaloa.edu.mx>
2024-03-11 14:11:43 -0700

    no es Gay

--- src/components/userlist.jsx
+++ src/components/userlist.jsx
@@ -1,43 +1,38 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { Avatar, Grid, Paper } from '@mui/material';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { Grid, Paper } from '@mui/material';
 
 const columns = [
   {
     field: 'nombre',
     headerName: 'Nombre',
-    width: 100,
+    width: 100,  
     editable: false,
   },
   {
     field: 'rol',
     headerName: 'Rol',
     sortable: false,
-    width: 120
+    width: 120,
   },
 ];
 
 const rows = [
   {id:1, nombre: 'Diego', rol: 'Adminstrador' },
   {id:2, nombre: 'Francisco', rol: 'Inspector' },
-  {id:3, nombre: 'LuisGay', rol: 'Administrador' },
+  {id:3, nombre: 'Luis', rol: 'Administrador' },
   {id:4, nombre: 'jisus', rol: 'Inspector' },
 
 ];
 const paperStyle={padding:20, height: '70vh', width: 280, margin: "20px auto"}
-const avatarStyle={backgroundColor:"orange"}
-
 
 export default function UserList() {
   return (
     <Grid>
     <Paper elevation={10} style={paperStyle}>
     <Grid align="center">
-                    <Avatar style={avatarStyle}><AccountCircleOutlinedIcon/></Avatar>
-                    <h2>Lista de Usuarios</h2>
-                </Grid>
+        <h2>Lista de Usuarios</h2>
+    </Grid>
       <DataGrid
         rows={rows}
         columns={columns}
