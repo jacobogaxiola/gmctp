043583e5ad55fcec08961dd7480f8064824f86f2
jacobogaxiola <jacobogaxiola@gmail.com>
2024-03-12 15:45:35 -0700

    Estilos y traducción al español

--- src/components/userlist.jsx
+++ src/components/userlist.jsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, esES } from '@mui/x-data-grid';
 import { Grid, Paper } from '@mui/material';
+
 
 const columns = [
   {
     field: 'nombre',
-    headerName: 'Nombre',
-    width: 100,  
-    editable: false,
+    headerClassName: 'datagridHeader',
+    headerName: 'Usuario',
+    width: 160,  
+    editable: false
   },
   {
     field: 'rol',
+    headerClassName: 'datagridHeader',
     headerName: 'Rol',
-    sortable: false,
-    width: 120,
+    width: 118,  
+    editable: false
   },
 ];
 
@@ -22,32 +25,35 @@ const rows = [
   {id:2, nombre: 'Francisco', rol: 'Inspector' },
   {id:3, nombre: 'Luis Gael', rol: 'Administrador' },
   {id:4, nombre: 'jisus', rol: 'Inspector' },
-
+  {id:5, nombre: 'Isis', rol: 'Inspector' },
+  {id:6, nombre: 'Lenin', rol: 'Inspector' },
+  {id:7, nombre: 'Paulina', rol: 'Inspector' },
+  {id:8, nombre: 'Jacobo', rol: 'Inspector' },
+  {id:9, nombre: 'Yan', rol: 'Inspector' }
 ];
 const paperStyle={padding:20, height: '70', width: 280, margin: "20px auto"}
 
 export default function UserList() {
   return (
     <Grid>
-    <Paper elevation={10} style={paperStyle}>
-    <Grid align="center">
-        <h2>Lista de Usuarios</h2>
-    </Grid>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+      <Paper elevation={10} style={paperStyle}>
+        <DataGrid
+          checkboxSelection=''
+          density='compact'
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 9,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Paper>
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+          localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+        />
+      </Paper>
    </Grid>
   );
 }
